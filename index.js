197552ccb3493b8c1e2ccfdad7691f0c122efe11
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-09-15 14:52:15 -0400

    finished up to task 3

--- index.js
+++ index.js
@@ -64,9 +64,6 @@ Person.prototype.toString = function(){
 
 
 
-
-
-
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -81,8 +78,15 @@ Person.prototype.toString = function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons){
+    this.tank = this.tank +gallons;
 }
 
 
@@ -93,11 +97,15 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
 
-
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
