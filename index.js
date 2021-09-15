d358d4050d2ceb22a6fdee29345bdfcb20a37ba2
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-09-15 14:19:57 -0400

    updated task 1

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age= age;
+  let stomach = [];
 }
 
 
